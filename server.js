eae3138d121cc3b27edce1a04b5fdca4db055943
Josh Akeman <joshakeman@gmail.com>
2019-04-24 13:39:24 -0400

    first commit

--- server.js
+++ server.js
@@ -1,18 +1,61 @@
 const express = require('express'); // importing a CommonJS module
+const helmet = require('helmet')
+const morgan = require('morgan')
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
-server.use(express.json());
+function greeter(teamName) {
+  return function(req, res, next) {
+    req.team = teamName
 
-server.use('/api/hubs', hubsRouter);
+    next()
+  }
+}
+
+function time(req, res, next) {
+    const seconds = new Date().getSeconds()
+    if (seconds % 3 === 0) {
+      return res.status(404).end()
+    } else {
+      next()
+    }
+}
+
+server.use(express.json());
+server.use(helmet())
+server.use(morgan('dev'))
+server.use(greeter('Web XVIII'))
+
+server.use('/api/hubs', restricted, only('gimli'), hubsRouter);
 
 server.get('/', (req, res, next) => {
   res.send(`
     <h2>Lambda Hubs API</h2>
-    <p>Welcome to the Lambda Hubs API</p>
+    <p>Welcome to ${req.team}</p>
     `);
 });
+
+function restricted(req, res, next) {
+  const password = req.headers.password
+
+  if (password === 'mellon') {
+    next()
+  } else {
+    res.status(401).send('you shall not pass!')
+  }
+}
+
+function only(name) {
+  return function name(req, res, next) {
+  const name = req.headers.name
+  if (name === 'gimli') {
+    next()
+  } else {
+    res.status(403).send("you're not gimli!")
+  }
+}
+}
 
 module.exports = server;
